5bbf4a89e3a1b7b48956849d6c1030c861398bec
Andrew Violette <aviolette@divvydose.com>
2020-12-02 07:11:28 -0600

    day2 - second solution

--- day2/puzzle2.py
+++ day2/puzzle2.py
@@ -13,11 +13,25 @@ class Rule:
         return self.lower <= count <= self.upper
 
 
-def count_valid_passwords(puzzle):
+class SecondRule:
+    def __init__(self, definition: str):
+        char_range, the_char = definition.split(" ")
+        self.pos1, self.pos2 = (
+            int(x.strip()) - 1 for x in char_range.strip().split("-")
+        )
+        self.the_char = the_char
+
+    def verify(self, password):
+        return (password[self.pos1] == self.the_char) != (
+            password[self.pos2] == self.the_char
+        )
+
+
+def count_valid_passwords(puzzle, rule_class):
     count = 0
-    for line in puzzle.readlines():
+    for line in lines:
         rule_spec, password = line.split(":")
-        rule = Rule(rule_spec.strip())
+        rule = rule_class(rule_spec.strip())
         if rule.verify(password.strip()):
             count = count + 1
     return count
@@ -25,4 +39,6 @@ def count_valid_passwords(puzzle):
 
 if __name__ == "__main__":
     puzzle = open("puzzle2.txt", "r")
-    print(count_valid_passwords(puzzle))
+    lines = [line for line in puzzle.readlines()]
+    print(count_valid_passwords(lines, Rule))
+    print(count_valid_passwords(lines, SecondRule))
--- day2/test_puzzle2.py
+++ day2/test_puzzle2.py
@@ -1,4 +1,4 @@
-from day2.puzzle2 import Rule
+from day2.puzzle2 import Rule, SecondRule
 
 
 def test_verify():
@@ -24,3 +24,18 @@ def test_verify_at_upper():
 def test_verify_above_upper():
     rule = Rule("7-11 l")
     assert not rule.verify("llllllllllll")
+
+
+def test_verify_rule_2_not():
+    rule = SecondRule("1-6 w")
+    assert not rule.verify("dwnwws")
+
+
+def test_verify_rule_2_pos1():
+    rule = SecondRule("1-6 w")
+    assert rule.verify("wwnwws")
+
+
+def test_verify_rule_2_pos2():
+    rule = SecondRule("1-6 w")
+    assert rule.verify("dwnwww")

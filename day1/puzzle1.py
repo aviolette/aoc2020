28f6392a9e9f29dd4c23fa250a07721d08b67a58
Andrew Violette <aviolette@divvydose.com>
2020-12-02 05:50:57 -0600

    Cleanup

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -1,11 +1,12 @@
-def combo_of_2020():
-    puzzle = open('puzzle.txt', 'r')
-    nums = [int(line.strip()) for line in puzzle.readlines()]
-    nums.sort()
+from functools import reduce
+from typing import List
+
+
+def combo_of_2020(nums: List[int]):
     upper = len(nums)
     for i in range(0, upper):
         x = nums[i]
-        for j in range(upper - 1, 0, -1):
+        for j in range(upper - 1, i, -1):
             y = nums[j]
             if x + y > 2020:
                 upper = j
@@ -14,26 +15,27 @@ def combo_of_2020():
     return None
 
 
-def three_of_2020():
-    puzzle = open('puzzle.txt', 'r')
-    nums = [int(line.strip()) for line in puzzle.readlines()]
-    nums.sort()
+def three_of_2020(nums: List[int]):
     upper = len(nums)
     for i in range(0, upper):
         x = nums[i]
-        for j in range(0, upper):
+        for j in range(i, upper):
             y = nums[j]
-            for l in range(0, upper):
+            for l in range(j, upper):
                 z = nums[l]
                 if x + y + z == 2020:
                     return x, y, z
     return None
 
 
-if __name__ == '__main__':
-    tup = combo_of_2020()
+if __name__ == "__main__":
+    puzzle = open("puzzle.txt", "r")
+    nums = [int(line.strip()) for line in puzzle.readlines()]
+    nums.sort()
+
+    tup = combo_of_2020(nums)
     print(tup)
     print(tup[0] * tup[1])
-    tup = three_of_2020()
+    tup = three_of_2020(nums)
     print(tup)
-    print(tup[0] * tup[1] * tup[2])
+    print(reduce(lambda x, y: x * y, tup))

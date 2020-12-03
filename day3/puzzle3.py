df13e71be5ded248cd8461cd6437d680348e52f9
Andrew Violette <aviolette@divvydose.com>
2020-12-03 07:24:36 -0600

    solution 2

--- day3/puzzle3.py
+++ day3/puzzle3.py
@@ -1,4 +1,5 @@
 import math
+from functools import reduce
 
 
 def expand_line(line, position):
@@ -6,23 +7,46 @@ def expand_line(line, position):
     return line * max(math.ceil((position + 1) / len(line)), 1)
 
 
-def compute_trees(lines):
+def compute_trees(lines, right, down):
     tree_count = 0
     position = 0
+    down_counter = down
     for line in lines:
+        if down_counter != down:
+            down_counter += 1
+            continue
+        down_counter = 1
         real_line = expand_line(line, position)
         if real_line[position] == "#":
             tree_count += 1
-        position += 3
+        position += right
     return tree_count
 
 
 def compute_trees_from_file(file_name):
     puzzle = open(file_name, "r")
     lines = [line for line in puzzle.readlines()]
-    return compute_trees(lines)
+    return compute_trees(lines, right=3, down=1)
+
+
+def product_of_all_slopes(file_name):
+    puzzle = open(file_name, "r")
+    lines = [line for line in puzzle.readlines()]
+
+    return reduce(
+        lambda x, y: x * y,
+        (
+            compute_trees(lines, right=1, down=1),
+            compute_trees(lines, right=3, down=1),
+            compute_trees(lines, right=5, down=1),
+            compute_trees(lines, right=7, down=1),
+            compute_trees(lines, right=1, down=2),
+        ),
+    )
 
 
 if __name__ == "__main__":
     print(compute_trees_from_file("example.txt"))
     print(compute_trees_from_file("puzzle3.txt"))
+    print(product_of_all_slopes("example.txt"))
+    print(product_of_all_slopes("puzzle3.txt"))

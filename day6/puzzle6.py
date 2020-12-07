1fc3cc9c71d57c8a0acb50d864c0ae2b3a38fff0
Andrew Violette <aviolette@divvydose.com>
2020-12-06 20:01:25 -0600

    Refine puzzle 6

--- day6/puzzle6.py
+++ day6/puzzle6.py
@@ -1,35 +1,34 @@
 from functools import partial
 
 
-def anyone_answers(person: str, group_start: bool, group: set):
+def anyone_answers(person: str, group: set):
+    if group is None:
+        group = set()
     for answer in person:
         group.add(answer)
     return group
 
 
-def everyone_answers(person: str, group_start: bool, group: set):
+def everyone_answers(person: str, group: set):
     person_answers = {answer for answer in person}
     if group:
         return group.intersection(person_answers)
-    elif group_start:
+    elif group is None:
         return person_answers
-    return group
+    return set()
 
 
 def sum_of_answers(func, file_name: str):
     puzzle = open(file_name, "r")
-    group = set()
-    group_start = True
+    group = None
     acc = 0
     for line in puzzle.readlines():
         line = line.strip()
         if len(line):
-            group = func(line, group_start, group)
-            group_start = False
+            group = func(line, group)
         else:
             acc += len(group)
-            group = set()
-            group_start = True
+            group = None
 
     return len(group) + acc
 

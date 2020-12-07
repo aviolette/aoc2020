9cccf0e610447bc1ef450d83f8255e6250ba8412
Andrew Violette <aviolette@divvydose.com>
2020-12-07 07:28:31 -0600

    puzzle4 refactor

--- day4/passport.py
+++ day4/passport.py
@@ -1,5 +1,7 @@
 import re
 
+from elves import group_lines, striplines
+
 REQUIRED_FIELDS = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 
 
@@ -59,21 +61,9 @@ def is_valid(passport_data):
 
 
 def valid_passports(file_name):
-    puzzle = open(file_name, "r")
-    acc = ""
-    count = 0
-    for line in puzzle.readlines():
-        line = line.strip()
-        if len(line):
-            acc += " " + line
-        else:
-            if is_valid(acc):
-                count += 1
-            acc = ""
-    if acc and is_valid(acc):
-        count += 1
-
-    return count
+    return sum(
+        1 if is_valid(" ".join(group)) else 0 for group in group_lines(file_name)
+    )
 
 
 if __name__ == "__main__":
